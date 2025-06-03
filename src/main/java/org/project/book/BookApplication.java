6e0b121793c54e5f9547270291530c013d675913
uwu <uwu.fionia@gmail.com>
2025-06-03 23:17:36 +0800

    Revert "修改lombok版本为1.18.34,在pom文件中指定maven版本为3.8.1"
    This reverts commit 9b5896da703bfb7738da44317430ce3d0de2971d.

--- src/main/java/org/project/book/BookApplication.java
+++ src/main/java/org/project/book/BookApplication.java
@@ -1,16 +1,12 @@
 package org.project.book;
 
-import org.project.book.pojo.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigInteger;
 
 @SpringBootApplication
 public class BookApplication {
 
 	public static void main(String[] args) {
-		new Book().setId(new BigInteger("1"));
 		SpringApplication.run(BookApplication.class, args);
 	}
 
