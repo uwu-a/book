ba382487c6963c36c80b26d8810aa01ebdf3058e
uwu <uwu.fionia@gmail.com>
2025-06-04 13:23:50 +0800

    添加了图书查询服务类功能：按ID、ISBN；like模糊查询

--- src/main/java/org/project/book/service/BookService.java
+++ src/main/java/org/project/book/service/BookService.java
@@ -1,11 +1,28 @@
 package org.project.book.service;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.project.book.mapper.BookMapper;
 import org.project.book.pojo.Book;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @Service
 public class BookService extends ServiceImpl<BookMapper, Book> {
 
+    public Book queryByID(BigInteger id) {
+        return super.getById(id);
+    }
+
+    public Book queryByISBN(String ISBN) {
+        return super.getOne(Wrappers.<Book>lambdaQuery().eq(Book::getIsbn, ISBN));
+    }
+
+    public List<Book> queryByAnyKeyword(String keyword) {
+        return super.getBaseMapper()
+                .selectList(Wrappers.lambdaQuery(Book.class).like(Book::getTitle, keyword)
+                        .or().like(Book::getAuthor, keyword).or().like(Book::getPublisher, keyword));
+    }
 }
